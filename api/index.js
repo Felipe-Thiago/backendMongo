41efca8747c925d9a44384b4cd262e2a59c754fa
Felipe Thiago <felipethiagodasilva@gmail.com>
2024-03-12 19:14:10 -0300

    PUT mongoDB

---
+++ api/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import {config} from 'dotenv'
+config() //carrega as variáveis do .env
+
+const app = express()
+const {PORT} = process.env
+//Import das rotas da aplicação
+import RotasPrestadores from './routes/prestador.js'
+
+app.use(express.json()) //Habilita o parse do JSON
+//Rota de conteúdo público
+app.use('/', express.static('public'))
+//Removendo o X-powered-by por segurança
+app.disable('x-powered-by')
+//Configurando o favicon
+app.use('/favicon.ico', express.static('public/images/logo-api.png')) 
+
+//Rota default
+app.get('/api', (req, res)=> {
+    res.status(200).json({
+        message: 'API FATEC 100% funcional',
+        version: '1.0.0'
+    })
+})
+//Rotas da API
+app.use('/api/prestadores', RotasPrestadores)
+//Listen
+app.listen(PORT, function(){
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
